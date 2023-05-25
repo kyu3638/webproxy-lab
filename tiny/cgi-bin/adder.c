b2d777fd5dba3f186f96beafe93151e660226eef
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-05-26 01:46:08 +0900

    Update adder.c

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,7 +4,40 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) {
+int main(void)
+{
+  char *buf, *p, *method;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1 = 0, n2 = 0;
+
+  /* Extract the two arguments */
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  {
+    p = strchr(buf, '&');
+    *p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p + 1);
+    n1 = atoi(strchr(arg1, '=') + 1);
+    n2 = atoi(strchr(arg2, '=') + 1);
+  }
+
+  method = getenv("REQUEST_METHOD");
+
+  /* Make the response body */
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+          content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+
+  /* Generate the HTTP response */
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  if (strcasecmp(method, "HEAD") != 0)
+    printf("%s", content);
+  fflush(stdout); // 연구해봅시다.
+
   exit(0);
 }
 /* $end adder */
